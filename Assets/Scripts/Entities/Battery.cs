6701ff0a2d5e25fa39801663a891e04e96845787
YamagusukuFaym <56829392+FrancoYamaDav@users.noreply.github.com>
2022-04-02 11:34:50 -0300

    test

---
+++ Assets/Scripts/Entities/Battery.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour, IMagnetable
+{
+    public void OnMagnetism(PlayerController pc = null)
+    {
+        throw new System.NotImplementedException();
+    }
+}
